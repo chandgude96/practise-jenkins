ef327eb121339e7760cba018b4dfb07259c6dcdc
chandgude96 <nikhilchandgudeofficial@gmail.com>
2024-04-03 10:48:44 +0530

    first commit

---
+++ test_file_004.py
@@ -0,0 +1,11 @@
+class Test_credence:
+    def test_sub_002(self):
+        a = 30
+        b = 10
+        sub = a - b
+        print("the sub of a and b is:",sub)
+        if sub == 20:
+            assert True
+        else:
+            assert False
+
