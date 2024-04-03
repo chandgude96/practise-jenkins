279ad06719508b52af41e7b4d29a0acce9ea045f
chandgude96 <165660811+chandgude96@users.noreply.github.com>
2024-04-03 18:07:03 +0530

    Update test_file_003.py

--- test_file_003.py
+++ test_file_003.py
@@ -8,3 +8,13 @@ class Test_match:
             assert True
         else:
             assert False
+class Test_match:
+    def test_add_003(self):
+        a = 20
+        b = 20
+        sum = a + b
+        print("the sum is :",sum)
+        if sum == 40:
+            assert True
+        else:
+            assert False
